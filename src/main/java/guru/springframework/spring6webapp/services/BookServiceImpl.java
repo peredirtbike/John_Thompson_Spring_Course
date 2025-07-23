f9f42cd3e2639e37c3cf01c71e091809478dbfa4
Pere <peredirtbike@gmail.com>
2025-07-23 20:57:34 +0200

    Implement BookService and BookServiceImpl for book retrieval functionality

---
+++ src/main/java/guru/springframework/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6webapp.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.repositories.BookRepository;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
